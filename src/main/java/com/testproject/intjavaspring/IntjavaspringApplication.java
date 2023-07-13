98486b74673f710dc9d7259dbfc537dae6bb3f3c
JohnDev <johncroft2236@hotmail.com>
2023-07-13 18:48:36 -0300

    Project created

---
+++ src/main/java/com/testproject/intjavaspring/IntjavaspringApplication.java
@@ -0,0 +1,13 @@
+package com.testproject.intjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntjavaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntjavaspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testproject/intjavaspring/IntjavaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testproject.intjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntjavaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
